a93727ff6becceaea751d24ed4341e5946c8e42f
David J McClelland <davidjmcclelland@gmail.com>
2023-01-09 13:18:47 -0500

    lineChart properties todo items added to visualizer

--- src/components/Hook/Visualizer.js
+++ src/components/Hook/Visualizer.js
@@ -1,18 +1,33 @@
 import React, { useEffect, useState } from 'react';
-import { Card, List } from 'antd';
+import { Card } from 'antd';
 import LineChart from "./LineChart";
 
+// DONE: change legend to show device name instead of topic
+// DONE: put topic in Visualizer title
+// DONE: fix warning index.js:1 Warning: Failed prop type: The prop `data[0].id` is marked as required in `Ge`, but its value is `undefined`.
+// TODO: fix warning Error: <path> attribute d: Expected moveto path command ('M' or 'm'), "null".
+// TODO: format date
+// TODO: limit ticks to 2 or 4
+// TODO: format value scale to readable values
+// TODO: fix tooltips floating in chart
+// TODO: allow card to expand to fullscreen
+// TODO: toggle multiple series from MQTT source data
+
+let titleTopic = '';
+let deviceName;
 const Visualizer = ({ payload }) => {
   const [messages, setMessages] = useState([]);
   useEffect(() => {
     if (payload.topic) {
+      titleTopic = `${payload.topic} Visualizer`;
       let rawMessage = JSON.parse(payload.message);
-      let deviceName = rawMessage.deviceName;
+      deviceName = rawMessage.deviceName;
       console.log(deviceName);
       if (deviceName === 'Random-UnsignedInteger-Device') {
+        const chartDate = Date.now()
         let message = {
           y: rawMessage.readings[0].value,
-          x: Date.now(),
+          x: chartDate,
         };
         console.log(rawMessage);
 
@@ -22,9 +37,9 @@ const Visualizer = ({ payload }) => {
   }, [payload])
  
   return (
-    <Card title="Visualizer">
+    <Card title={titleTopic}>
       <div style={{ height: 200 }}>
-        <LineChart mqtt={messages} topic={payload.topic} />
+        <LineChart mqtt={messages} seriesName={deviceName} />
       </div>
     </Card>
   );
