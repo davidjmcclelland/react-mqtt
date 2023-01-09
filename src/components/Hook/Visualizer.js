6440013d9fcc024488ddd2ffd2eee25634710882
David J McClelland <davidjmcclelland@gmail.com>
2023-01-09 11:44:54 -0500

    chart outside of list adds points

--- src/components/Hook/Visualizer.js
+++ src/components/Hook/Visualizer.js
@@ -1,34 +1,31 @@
 import React, { useEffect, useState } from 'react';
 import { Card, List } from 'antd';
+import LineChart from "./LineChart";
 
 const Visualizer = ({ payload }) => {
-  const [messages, setMessages] = useState([])
-
+  const [messages, setMessages] = useState([]);
   useEffect(() => {
     if (payload.topic) {
-      setMessages(messages => [...messages, payload])
+      let rawMessage = JSON.parse(payload.message);
+      let deviceName = rawMessage.deviceName;
+      console.log(deviceName);
+      if (deviceName === 'Random-UnsignedInteger-Device') {
+        let message = {
+          y: rawMessage.readings[0].value,
+          x: Date.now(),
+        };
+        console.log(rawMessage);
+
+        setMessages((messages) => [...messages, message]);
+      }
     }
   }, [payload])
-
-  const renderListItem = (item) => (
-    <List.Item>
-      <List.Item.Meta
-        title={item.topic}
-        description={item.message}
-      />
-    </List.Item>
-  )
-
+ 
   return (
-    <Card
-      title="Visualizer"
-    >
-      <List
-        size="small"
-        bordered
-        dataSource={messages}
-        renderItem={renderListItem}
-      />
+    <Card title="Visualizer">
+      <div style={{ height: 200 }}>
+        <LineChart mqtt={messages} topic={payload.topic} />
+      </div>
     </Card>
   );
 }
